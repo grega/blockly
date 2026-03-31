50591e03e07fe0d4947d780f8fafb39c4f3abc38
Greg Annandale <greg@raspberrypi.org>
2026-03-31 12:24:01 +0100

    chore(docs): tidy Docusaurus config

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -1,33 +1,24 @@
 // @ts-check
 // `@type` JSDoc annotations allow editor autocompletion and type checking
 // (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
 
 import { themes as prismThemes } from 'prism-react-renderer';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Blockly Docs',
   favicon: 'img/logo.svg',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
   url: 'https://raspberrypifoundation.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'RaspberryPiFoundation', // Usually your GitHub org/user name.
-  projectName: 'blockly', // Usually your repo name.
+  // GitHub pages deployment config
+  organizationName: 'RaspberryPiFoundation',
+  projectName: 'blockly',
   
   onBrokenLinks: 'warn',
   //onBrokenMarkdownLinks: 'warn',
@@ -39,9 +30,6 @@ const config = {
     },
   },
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -65,8 +53,6 @@ const config = {
           routeBasePath: '/',
           sidebarPath: './sidebars.js',
           showLastUpdateTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/RaspberryPiFoundation/blockly/tree/main/packages/docs',
         },
@@ -74,7 +60,7 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
-        // Will be passed to @docusaurus/plugin-google-tag-manager (only enabled when explicitly specified)
+        // Passed to @docusaurus/plugin-google-tag-manager
         googleTagManager: {
           containerId: 'GTM-NSSCB6XT',
         },
@@ -85,7 +71,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: 'images/blockly_banner.png',
       navbar: {
         title: 'Blockly',
